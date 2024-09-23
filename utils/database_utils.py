44c3adef39bb0b8d5e2eb23c8005dfae6fe60643
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-23 22:22:18 +0530

    Update database_utils.py

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -14,6 +14,16 @@ def get_db_engine():
     engine = create_engine(db_url)
     return engine
 
+def fetch_hull_performance_data(vessel_name, engine):
+    query = f"""
+    SELECT vessel_name, report_date, hull_roughness_power_loss
+    FROM hull_performance
+    WHERE UPPER(vessel_name) = '{vessel_name.upper()}'
+    """
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        return result.fetchall()
+
 def fetch_speed_consumption_data(vessel_name, engine):
     query = f"""
     SELECT vessel_name, report_date, speed, normalised_consumption, loading_condition
@@ -23,5 +33,3 @@ def fetch_speed_consumption_data(vessel_name, engine):
     with engine.connect() as connection:
         result = connection.execute(query)
         return result.fetchall()
-
-# Add other database utility functions here
