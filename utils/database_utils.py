95af7db372f8f07ee31389c3e4287181d8f6c5fc
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-23 21:41:12 +0530

    Create database_utils.py

---
+++ utils/database_utils.py
@@ -0,0 +1,27 @@
+import urllib.parse
+from sqlalchemy import create_engine
+import streamlit as st
+
+def get_db_engine():
+    supabase_host = st.secrets["supabase"]["host"]
+    supabase_database = st.secrets["supabase"]["database"]
+    supabase_user = st.secrets["supabase"]["user"]
+    supabase_password = st.secrets["supabase"]["password"]
+    supabase_port = st.secrets["supabase"]["port"]
+
+    encoded_password = urllib.parse.quote(supabase_password)
+    db_url = f"postgresql+psycopg2://{supabase_user}:{encoded_password}@{supabase_host}:{supabase_port}/{supabase_database}"
+    engine = create_engine(db_url)
+    return engine
+
+def fetch_speed_consumption_data(vessel_name, engine):
+    query = f"""
+    SELECT vessel_name, report_date, speed, normalised_consumption, loading_condition
+    FROM hull_performance
+    WHERE UPPER(vessel_name) = '{vessel_name.upper()}'
+    """
+    with engine.connect() as connection:
+        result = connection.execute(query)
+        return result.fetchall()
+
+# Add other database utility functions here
