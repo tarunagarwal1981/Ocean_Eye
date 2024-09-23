24b63e73aff7a9ff5252fd0b08effeb9943be1db
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-23 22:06:20 +0530

    Update speed_consumption_agent.py

--- agents/speed_consumption_agent.py
+++ agents/speed_consumption_agent.py
@@ -1,6 +1,6 @@
 from .base_agent import Agent
-from utils.database_utils import fetch_speed_consumption_data
-from utils.nlp_utils import clean_vessel_name, extract_vessel_name, get_llm_analysis
+from ..utils.database_utils import fetch_hull_performance_data
+from ..utils.nlp_utils import clean_vessel_name, extract_vessel_name, get_llm_analysis
 
 class SpeedConsumptionAgent(Agent):
     def process_query(self, query: str, engine):
