f315e5bd1b4856c2cbeaa5b6ba6de8f5b8d04074
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-25 18:25:12 +0530

    Update database_utils.py

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -14,22 +14,30 @@ def get_db_engine():
     engine = create_engine(db_url)
     return engine
 
-def fetch_hull_performance_data(vessel_name, engine):
+def fetch_hull_performance_data(vessel_name, engine, from_date=None, to_date=None):
     query = f"""
     SELECT vessel_name, report_date, hull_roughness_power_loss
     FROM hull_performance
     WHERE UPPER(vessel_name) LIKE '%{vessel_name.upper().replace("'", "''")}%'
     """
+    
+    if from_date and to_date:
+        query += f" AND report_date BETWEEN '{from_date}' AND '{to_date}'"
+    
     with engine.connect() as connection:
         result = connection.execute(query)
         return result.fetchall()
 
-def fetch_speed_consumption_data(vessel_name, engine):
+def fetch_speed_consumption_data(vessel_name, engine, from_date=None, to_date=None):
     query = f"""
     SELECT vessel_name, report_date, speed, normalised_consumption, loading_condition
     FROM hull_performance
     WHERE UPPER(vessel_name) LIKE '%{vessel_name.upper().replace("'", "''")}%'
     """
+    
+    if from_date and to_date:
+        query += f" AND report_date BETWEEN '{from_date}' AND '{to_date}'"
+    
     with engine.connect() as connection:
         result = connection.execute(query)
         return result.fetchall()
