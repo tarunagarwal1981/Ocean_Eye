4f067d4cfe8793b5cf29b54bbc0b3b137156819b
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-25 18:27:54 +0530

    Update database_utils.py

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -1,5 +1,5 @@
 import urllib.parse
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import streamlit as st
 
 def get_db_engine():
@@ -15,29 +15,37 @@ def get_db_engine():
     return engine
 
 def fetch_hull_performance_data(vessel_name, engine, from_date=None, to_date=None):
-    query = f"""
+    query = """
     SELECT vessel_name, report_date, hull_roughness_power_loss
     FROM hull_performance
-    WHERE UPPER(vessel_name) LIKE '%{vessel_name.upper().replace("'", "''")}%'
+    WHERE UPPER(vessel_name) LIKE :vessel_name
     """
     
+    params = {'vessel_name': f'%{vessel_name.upper()}%'}
+    
     if from_date and to_date:
-        query += f" AND report_date BETWEEN '{from_date}' AND '{to_date}'"
+        query += " AND report_date BETWEEN :from_date AND :to_date"
+        params['from_date'] = from_date
+        params['to_date'] = to_date
     
     with engine.connect() as connection:
-        result = connection.execute(query)
+        result = connection.execute(text(query), params)
         return result.fetchall()
 
 def fetch_speed_consumption_data(vessel_name, engine, from_date=None, to_date=None):
-    query = f"""
+    query = """
     SELECT vessel_name, report_date, speed, normalised_consumption, loading_condition
     FROM hull_performance
-    WHERE UPPER(vessel_name) LIKE '%{vessel_name.upper().replace("'", "''")}%'
+    WHERE UPPER(vessel_name) LIKE :vessel_name
     """
     
+    params = {'vessel_name': f'%{vessel_name.upper()}%'}
+    
     if from_date and to_date:
-        query += f" AND report_date BETWEEN '{from_date}' AND '{to_date}'"
+        query += " AND report_date BETWEEN :from_date AND :to_date"
+        params['from_date'] = from_date
+        params['to_date'] = to_date
     
     with engine.connect() as connection:
-        result = connection.execute(query)
+        result = connection.execute(text(query), params)
         return result.fetchall()
