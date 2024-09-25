95c743f6f1d230df94d976e35fa76178902f9b55
tarunagarwal1981 <49944511+tarunagarwal1981@users.noreply.github.com>
2024-09-25 21:42:08 +0530

    Update database_utils.py

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -1,13 +1,15 @@
-import pandas as pd
-import psycopg2
 import os
+import psycopg2
+import pandas as pd
 
 def fetch_data_from_db(query: str) -> pd.DataFrame:
+    # Current code using environment variables or hardcoded values (e.g., os.getenv)
     conn = psycopg2.connect(
         host=os.getenv('SUPABASE_HOST'),
         database=os.getenv('SUPABASE_DB'),
         user=os.getenv('SUPABASE_USER'),
-        password=os.getenv('SUPABASE_PASSWORD')
+        password=os.getenv('SUPABASE_PASSWORD'),
+        port=os.getenv('SUPABASE_PORT')
     )
     try:
         data = pd.read_sql(query, conn)
